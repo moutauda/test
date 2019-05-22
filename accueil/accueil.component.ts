dd349f54090075004a852f5156c9d31db98033d4
moutauda <33935975+moutauda@users.noreply.github.com>
2019-05-22 17:17:29 +0200

    add acceuil

---
+++ accueil/accueil.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'accueil',
+  templateUrl: './accueil.component.html',
+  styleUrls: ['./accueil.component.scss']
+})
+export class AccueilComponent implements OnInit {
+
+
+  let
+
+  constructor() { }
+
+  ngOnInit() {
+    this.getVictory();
+    this.getTeams();
+  }
+
+
+  getVictory () {
+
+  }
+
+  getTeams () {
+
+}
+
+}
