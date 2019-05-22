7fa5b11caece6407d41626989020386b87fd7228
moutau_a <alexandre.moutaud@epitech.eu>
2019-05-22 23:12:28 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule } from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
+import { MonPremierComponent } from './mon-premier/mon-premier.component'
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { AppareilComponent } from './appareil/appareil.component';
-import { AppareilService } from './services/appareil.service';
+//import { AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import {RouterModule, Routes} from '@angular/router';
-import { AuthService } from './services/auth.service';
+//import { AuthService } from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
-import { AuthGuard } from './services/auth-guard.service';
+//import { AuthGuard } from './services/auth-guard.service';
 import {ResultatsService} from "./services/resultats.service";
 
 const appRoutes: Routes = [
@@ -26,6 +27,11 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
+    AppareilComponent,
+    AppareilViewComponent,
+    AuthComponent,
+    MonPremierComponent,
+    SingleAppareilComponent,
     AccueilComponent,
     FourOhFourComponent
   ],
