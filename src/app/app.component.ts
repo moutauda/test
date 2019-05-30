d27fd0570a693068e79fcb4083add77d6c830db2
moutau_a <alexandre.moutaud@epitech.eu>
2019-05-30 15:51:16 +0200

    goooo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,74 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
-import { Subscription } from 'rxjs/Rx';
+import { Component, OnInit } from '@angular/core';
+import data from '../assets/SampleJson.json';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-
-  secondes: number;
-  counterSubscription: Subscription;
-
-  constructor() {}
-
+export class AppComponent implements OnInit {
+  resultats = undefined;
 
   ngOnInit() {
-    const counter = Observable.interval(1000);
-    this.counterSubscription = counter.subscribe(
-      (value: number) => {
-        this.secondes = value;
-      }
-    );
+    this.resultats = (<any>data.resultats);
+    console.log(this.resultats);
+    console.log(this.teams);
+    this.getVictory();
+    this.getTeams();
+    this.getNbmatchLeft();
   }
 
-  ngOnDestroy() {
-    this.counterSubscription.unsubscribe();
+  teams = ['Usa', 'Mexique', 'Colombie', 'Bolivie', 'Hollande', 'Belgique', 'Irlande',
+    'Espagne', 'Maroc', 'Brésil', 'Argentine', 'Allemagne', 'Suisse', 'Nigeria',
+    'Italie', 'Cameroun', 'Norvège', 'Suède', 'Bulgarie', 'Roumanie', 'Grèce',
+    'Arabie Saoudite', 'Russie', 'Corée', 'Japon', 'France', 'Angleterre', 'Uruguay']
+
+  vicJ1 = 0;
+  vicJ2 = 0;
+
+  teamJ1 = [];
+  teamJ2 = [];
+
+  leftTeamJ1 = [];
+  leftTeamJ2 = [];
+
+  matchRestants = 0;
+
+  getVictory() {
+    for (let i = 0; i < this.resultats.length; i++) {
+      if (this.resultats[i].score1 > this.resultats[i].score2) {
+        this.vicJ1 += 1;
+      } else {
+        this.vicJ2 += 1;
+      }
+    }
+    console.log('nb victoire j1 : ' + this.vicJ1);
+    console.log('nb victoire j2 : ' + this.vicJ2);
+  }
+
+  getTeams() {
+    for (let i = 0; i < this.resultats.length; i++) {
+      this.teamJ1.push(this.resultats[i].equipe1);
+      this.teamJ2.push(this.resultats[i].equipe2);
+    }
+    console.log(this.teamJ1);
+    console.log(this.teamJ2);
+
+    for (let i = 0; i < this.teams.length; i++) {
+      if (!this.teamJ1.includes(this.teams[i])) {
+        this.leftTeamJ1.push(this.teams[i]);
+      }
+      if (!this.teamJ2.includes(this.teams[i])) {
+        this.leftTeamJ2.push(this.teams[i]);
+      }
+    }
+    console.log(this.teams.length);
+    console.log(this.leftTeamJ1);
+    console.log(this.leftTeamJ2);
+  }
+
+  getNbmatchLeft(){
+    this.matchRestants = this.teams.length - (this.vicJ1 + this.vicJ2)
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule } from '@angular/forms'
 
-//import { AppRoutingModule } from './app-routing.module';
-import { MonPremierComponent } from './mon-premier/mon-premier.component'
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccueilComponent } from './accueil/accueil.component';
-import { AppareilComponent } from './appareil/appareil.component';
-//import { AppareilService } from './services/appareil.service';
-import { AuthComponent } from './auth/auth.component';
-import { AppareilViewComponent } from './appareil-view/appareil-view.component';
-import {RouterModule, Routes} from '@angular/router';
-//import { AuthService } from './services/auth.service';
-import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
-import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
-//import { AuthGuard } from './services/auth-guard.service';
-import {ResultatsService} from "./services/resultats.service";
-
-const appRoutes: Routes = [
-  { path: '', component : AccueilComponent},
-  { path: 'not-found', component : FourOhFourComponent},
-  { path: '**', redirectTo: '/not-found' }
-];
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AppareilComponent,
-    AppareilViewComponent,
-    AuthComponent,
-    MonPremierComponent,
-    SingleAppareilComponent,
-    AccueilComponent,
-    FourOhFourComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule,
-    RouterModule.forRoot(appRoutes),
-    FormsModule
+    AppRoutingModule
   ],
-  providers: [
-      ResultatsService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
